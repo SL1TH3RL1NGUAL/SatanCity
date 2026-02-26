b9057d4e46b0696d6948f9bb6a69a670e038e1ac
Luciio <message.luciio@gmail.com>
2026-02-25 18:16:10 -0600

    Add backend and package.json

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ status: "online", domain: "api.blackcorp.me" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("API running");
+});
